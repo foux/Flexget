8683203f55fe1357ae1fa630eaaae992425e8226
François-Xavier Payet <fx.payet@tfdn.org>
2013-12-09 06:48:51 +0100

    Work in progress

--- flexget/plugins/output/subliminal_download.py
+++ flexget/plugins/output/subliminal_download.py
@@ -1,7 +1,10 @@
 from __future__ import unicode_literals
 
 import logging
-from flexget.plugin import register_plugin
+from flexget.plugin import register_plugin, DependencyError
+from babelfish.language import Language
+import locale
+import os
 
 log = logging.getLogger("subliminal_download");
 
@@ -20,20 +23,50 @@ class SubliminalDownload(object):
             'providers' : {
                 'type' : 'object',
                 'properties' : {
-                    'opensubtitles' : {'type' : 'boolean', 'default' : 'true'},
-                    'thesubdb' : {'type' : 'boolean', 'default' : 'true'},
-                    'podnapisi' : {'type' : 'boolean', 'default' : 'true'},
-                    'addic7ed' : {'type' : 'boolean', 'default' : 'false'},
-                    'tvsubtitles' : {'type' : 'boolean', 'default' : 'true'}
+                    'opensubtitles' : {'type' : 'boolean', 'default' : True},
+                    'thesubdb' : {'type' : 'boolean', 'default' : True},
+                    'podnapisi' : {'type' : 'boolean', 'default' : True},
+                    'addic7ed' : {'type' : 'boolean', 'default' : False},
+                    'tvsubtitles' : {'type' : 'boolean', 'default' : True}
                 },
                 'additionalProperties' : False
             },
-            'minimum_score' : { 'type' : 'number', 'default' : '0.0'},
-            'hearing_imparaired' : { 'type' : 'boolean', 'default' : 'false'}
+            'minimum_score' : { 'type' : 'number', 'default' : 0.0},
+            'hearing_imparaired' : { 'type' : 'boolean', 'default' : False}
         },
         'additionalProperties' : False
     }
     
-    def process_config(self, config):
+    def on_task_start(self, task, config):
+        try:
+            import subliminal
+        except ImportError as e:
+            log.debug('Error importing subliminal : %s' % e)
+            raise DependencyError('subliminal', 'subliminal', 'Subliminal module required for this task')
+        
+    def on_task_output(self, task, config):
+        import subliminal
+        languages = {
+            Language(list(locale.getdefaultlocale())[1][0:2])
+        }
+        for entry in task.accepted:
+            if not entry['location']:
+                entry.reject('no location for entry')
+            elif not os.path.exists(entry['location']):
+                entry.reject('file not found')
+            else:
+                videos = subliminal.scan_video(entry['location'])
+                
+                if videos is None:
+                    entry.reject('No videos found in entry')
+                else:
+                    subtitles = subliminal.download_best_subtitles(videos,
+                                                                   config['languages'],
+                                                                   config['accepted_providers'],
+                                                                   provider_configs=None,
+                                                                   min_score=config['minimum_score'],
+                                                                   hearing_impaired=config['hearing_impaired'])
+                    
+                
     
-register_plugin(SubliminalDownload, "subliminal_download",api_ver=2)
+register_plugin(SubliminalDownload, "subliminal_download", api_ver=2)
